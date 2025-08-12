6d9272a87e47b0247bd5b73fc2cbdb6be660fce3
munucrafts <sarthakmishra520@gmail.com>
2025-08-12 20:47:27 +0530

    Some Restructuring

--- Source/Get_Thumbnails/Private/BFL_GetThumbnails.cpp
+++ Source/Get_Thumbnails/Private/BFL_GetThumbnails.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BFL_GetThumbnails.h"
 #include <string>
 #include <AssetRegistry/AssetRegistryModule.h>
@@ -19,47 +16,39 @@ void UBFL_GetThumbnails::SaveThumbnail(const FAssetData& AssetData)
 
 	if (FPackageName::DoesPackageExist(AssetData.PackageName.ToString(), &PackageFilename))
 	{
-		// Getting the asset registry module
 		FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-
-		// Getting the asset registry
 		IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
 
-		// Loading the Thumbnails from Above Created Vars (PackageFileName, AssetFullNames) and Filling the Map (ThumbnailMap)
 		FThumbnailMap ThumbnailMap;
 		ThumbnailTools::LoadThumbnailsFromPackage(PackageFilename, AssetFullNames, ThumbnailMap);
-
-		// Finding the Asset's Thumbnail from the Now Filled Map Var (ThumbnailMap)
 		FObjectThumbnail* ObjectThumbnail = ThumbnailMap.Find(AssetFullName);
 
-		// Loading the ImageWrapperModule and Creating an ImageWrapper Var
 		IImageWrapperModule& ImageWrapperModule = FModuleManager::Get().LoadModuleChecked<IImageWrapperModule>(TEXT("ImageWrapper"));
 		TSharedPtr<IImageWrapper> ImageWrapper = ImageWrapperModule.CreateImageWrapper(EImageFormat::PNG);
 
-		// Adding the ObjectThumbnail on the Image Wrapper and Compressing it to a ByteArray
-		ImageWrapper->SetRaw(ObjectThumbnail->GetUncompressedImageData().GetData(), ObjectThumbnail->GetUncompressedImageData().Num(), ObjectThumbnail->GetImageWidth(), ObjectThumbnail->GetImageHeight(), ERGBFormat::BGRA, 8);
+		ImageWrapper->SetRaw(ObjectThumbnail->GetUncompressedImageData().GetData(), 
+							 ObjectThumbnail->GetUncompressedImageData().Num(), 
+							 ObjectThumbnail->GetImageWidth(), 
+							 ObjectThumbnail->GetImageHeight(), ERGBFormat::BGRA, 8);
+
 		const TArray64<uint8>& CompressedByteArray = ImageWrapper->GetCompressed();
 
-		// Save the texture to Thumbnails Folder
 		FString ThumbnailSavePath = (FPaths::ProjectDir() + "Thumbnails");
 		FPlatformFileManager::Get().GetPlatformFile().CreateDirectoryTree(*ThumbnailSavePath);
 		FString ImageFilename = AssetData.AssetName.ToString() + ".png";
 		FString CompleteImageFilePath = FPaths::Combine(ThumbnailSavePath, ImageFilename);
 		FFileHelper::SaveArrayToFile(CompressedByteArray, *CompleteImageFilePath);
-		RemoveBackgroundUsingPythonScript(CompleteImageFilePath);
-	}
+
+		RemoveThumbnailBackground(CompleteImageFilePath);
+	} 
 }
 
-void UBFL_GetThumbnails::RemoveBackgroundUsingPythonScript(FString AssetPath)
+void UBFL_GetThumbnails::RemoveThumbnailBackground(const FString& AssetPath)
 {
-	// Path to the Python interpreter and script
 	FString ProjectDir = FPaths::ProjectDir();
-	FString PythonScriptPath = ProjectDir / TEXT("Plugins/CPP-UE-GetThumbnails/Content/Python/RemoveThumbnail_BG.py");
-
-	// Command to execute the Python script
+	FString PythonScriptPath = ProjectDir / TEXT("Plugins/CPP-UE-GetThumbnails/Source/Python/RemoveBackground.py");
 	FString PythonInterpreter = TEXT("python");
 	FString Command = FString::Printf(TEXT("\"%s\" \"%s\""), *PythonScriptPath, *AssetPath);
 
-	// Execute the command as a subprocess
 	FPlatformProcess::CreateProc(*PythonInterpreter, *Command, true, false, false, nullptr, 0, nullptr, nullptr);
 }
--- Source/Get_Thumbnails/Public/BFL_GetThumbnails.h
+++ Source/Get_Thumbnails/Public/BFL_GetThumbnails.h
@@ -1,22 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "BFL_GetThumbnails.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class GET_THUMBNAILS_API UBFL_GetThumbnails : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 	
-	UFUNCTION(BlueprintCallable, Category = "SaveThumbnails")
+	UFUNCTION(BlueprintCallable)
 	static void SaveThumbnail(const FAssetData& AssetData);
 
 	UFUNCTION()
-	static void RemoveBackgroundUsingPythonScript(FString AssetPath);
+	static void RemoveThumbnailBackground(const FString& AssetPath);
 };
